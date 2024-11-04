97017e053d7ec936a3d11fd3d5942e2b5766fa04
Hassinw <hassinwilliams1@gmail.com>
2024-11-04 18:57:53 +0000

    last friday async activitie answered simple questions

---
+++  asyncActivity_nov1.js
@@ -0,0 +1,16 @@
+// the difference between varuniversial variable/ data container . let is  a variable that is expected to change const is  a variable that will not change. stays constant
+ let a = 2000
+ let b = 1000 
+ let c = 1000
+
+ // combine variables that when combined will be 4000
+ 
+ 
+ // is a function where when you put a variabgle inside of the round brackets, it will show the value in the terminal 
+ console.log(a)
+ console.log(b)
+ console.log(c)
+
+ // a scenerio when you would use the let varaible is when you want to but something ina container. and when you want to use const when whenever you want to declare a variable whose value should not be reassigned
+  
+// three things that would improve this class would be less talking more group woirk and class treats for when we do a good job
