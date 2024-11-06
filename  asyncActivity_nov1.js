5b6f83aff5ec7269f3e12bb1cf448ccb028138c4
Hassinw <hassinwilliams1@gmail.com>
2024-11-06 20:10:01 +0000

    reviwwed class asyncactivity

---  asyncActivity_nov1.js
+++  asyncActivity_nov1.js
@@ -1,16 +1,22 @@
-// the difference between varuniversial variable/ data container . let is  a variable that is expected to change const is  a variable that will not change. stays constant
- let a = 2000
- let b = 1000 
- let c = 1000
+ //  1. the difference between var,let,const
 
- // combine variables that when combined will be 4000
+"var - standard variable. short for variable. this can be used in any context   "
+"let -this a varible where the data can change."
+"const - this a vara=ibale where the data need to remain constant. canot change"
+
+
+
+// 2. create 3 variable that multiply into 4000
+var a = 2
+var b = 2
+var c = 1000
+console.log(a* b* c);
+
+
  
  
- // is a function where when you put a variabgle inside of the round brackets, it will show the value in the terminal 
- console.log(a)
- console.log(b)
- console.log(c)
+ 
 
- // a scenerio when you would use the let varaible is when you want to but something ina container. and when you want to use const when whenever you want to declare a variable whose value should not be reassigned
+ //  4. a scenerio when you would use the let varaible is when you want something to be a draft like an email. and when you want to use const when you use your final draft and want it to be permanant
   
-// three things that would improve this class would be less talking more group woirk and class treats for when we do a good job
+// 5. three things that would improve this class would be less talking more group woirk and class treats for when we do a good job.
