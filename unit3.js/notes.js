e2df5b97d7ad60a083dd6874540eaaf3daf13b59
Hassinw <hassinwilliams1@gmail.com>
2024-11-07 20:11:46 +0000

    naming varaible and added notes

--- unit3.js/notes.js
+++ unit3.js/notes.js
@@ -13,8 +13,8 @@ console.log
 12
 123323
 940.00
-7657.876878 
- 
+7657.876878
+
 
 // nolean
 // booleans represemt data that has a true of false value
@@ -25,29 +25,55 @@ console.log
 // the programs memory
 
 // universial variable/ data container
-var name= hassin 
+var name = hassin
 
 // a variable that is expected to change 
-let loginame= 'hassin';
+let loginame = 'hassin';
 
 // a variable that will not change. stays constant
 const password'mootmoot38';
 
- // varibles are container for data
+// varibles are container for data
 
 // sysntax- hown we write and format our code.
 // when we do not follow the format rules, jsut like in english
+
 // it can be hard for the computer 
 // to understand what we want it to do
 "hell0, my name is hassin, is today weather good"
 
-// when we follow the syntax rules, ju8st like in the
+//when we follow the syntax rules, ju8st like in the
+
+
 // world, we can be understood
 "hello my name is hassin, today there is pretty good weather"
 
 // warning and arrors
-varnumber=10;
+varnumber = 10;
 console.log('this is the termanal')
-var weather_city ='philly' //universal varible keyboard.
+var weather_city = 'philly' //universal varible keyboard.
 let weather_temp
-const main_city_weather='philly'
+const main_city_weather = 'philly'
+
+
+
+var example = ' this a string '// anything wrapped in this a qoute 
+var example = true // boolean - true and false
+var axample = 12345 /// integers = numbers 
+
+// variable should be short, simple and descriptive 
+
+var //is universal keyword for creating a variable or const/lete
+
+// keyword is a word built itno the js language
+
+let='' // special usage- data is expected to change.
+const-'' // special usage dasta is expected nit to change
+
+// cmael case = every word after the first is capitilized 
+tiktokaccount= 'moo101'
+// snake case = every word is seperated by an underscore 
+var tiktok_account_name =' idawgedyouho'
+
+// pascal case = every word is capitilized.
+var tiktokaccountname= 'mootmoot38'
