76c55581ab3adc9b6070e4771cba7193795b91f6
Hassinw <hassinwilliams1@gmail.com>
2024-11-18 18:58:43 +0000

    friday async activty

--- unit3.js/asyncActivityNov11.js
+++ unit3.js/asyncActivityNov11.js
@@ -1,4 +1,32 @@
 ' The Assignment Operator = assigns values examples '
+
 ' == operator is known as the loose equality operator. It compares two values for equality, and it performs type coercion if necessary.'
+
 ' the && operator is used to combine two mor more expressions and evaluate true'
+
 ' this  || the logical operator this it  It used to perform a logical OR operation on two operands'
+
+var username ='' 
+var passwor =''
+
+var securitySystemusername ='hassin'
+var securitySystempassword ='124'
+ 
+console.Log()
+
+var messageToSend= 'hello class'
+var messageToSen= messageToSend
+
+'single equal sign:'
+
+// =
+'single equal sign: assigment operators'
+
+// &&
+'double ampersand: Logical and operator'
+
+// ||
+' double vertuical slash: logical or operator'
+
+// ==
+"double equal: comparison SAME/EQUAL operator"
