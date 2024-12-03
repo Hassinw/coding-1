bb9ac07c9a1315c2dca55918c2f26eff4d2e1b18
Hassinw <hassinwilliams1@gmail.com>
2024-12-03 20:17:34 +0000

    today activity made pizza invitation list

---
+++ unit 3 .js/handsOnFunction.js
@@ -0,0 +1,36 @@
+// anatomy of a function
+
+// curly brackets = this is called
+
+// called parameter. we pass date
+
+// insdie of the functions parameters
+
+// parameters = placeceholders for data type
+
+// data = just means data types
+
+function pizzaParty(name, topping) {
+    console.log('congrats ' + name + 'you have been invited to a pizza party ')
+    console.log('your coustom pizza will have' + topping + 'on it');
+}
+pizzaParty('Mikle ','veggies ')
+
+
+// Conditional Statments in Js
+
+
+// written using the "if" and "else" keywwords this allowed 
+// the ability to make decjsjons based on data
+// our functions reveives.
+
+function pizzaPartyIncitation(eithan) {
+    let guest = 'miah'
+    if (name = guest){
+        console.log('this name is not on the list');
+    }else{
+        console.log("welcome to the party");
+    }
+}
+pizzaPartyinvitation(Hassin)
+
