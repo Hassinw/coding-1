94e9a04a8c12ee5d1c4088d5003347706aa663f4
Hassinw <hassinwilliams1@gmail.com>
2024-12-09 18:58:56 +0000

    friday async activy worked on in for today class

---
+++ unit 3 .js/asyncActivityDec6.js
@@ -0,0 +1,27 @@
+function reverse (pineapple) 
+
+
+
+
+
+
+
+
+function stateoflandmark (philly){
+    let = landmark 'philly'
+    if (landmark= philly){
+        console.log('Philly is nuturious for their cheesestakes')
+    }
+}
+
+
+
+
+
+function pizzaPartyIncitation(eithan) {
+    let guest = 'miah'
+    if (name = guest) {
+        console.log('this name is not on the list');
+    } else {
+        console.log("welcome to the party");
+    }
