72fe9b5a9f5b756accb534f38c8f2e3020069dae
Hassinw <hassinwilliams1@gmail.com>
2024-12-13 20:15:42 +0000

    today async activty worked on some and got to the 2nd question

---
+++ unit 3 .js/ asyncActivityDec13.js
@@ -0,0 +1,32 @@
+
+function negitivenumbers(number) {
+    console.log('coding is working')
+    console.log(number)
+    if (number >= 1) {
+        console.log('this positive number');
+    } else if (number < 0) {
+        console.log('this is a negitve number');
+
+    } else if (number == 0) {
+        console.log('this is the number 0.')
+    }
+    else {
+        console.log('sorry this is not a number')
+    }
+
+
+}
+
+negitivenumbers(7)
+
+
+function employediscount(name, number) {
+    console.log('james ' + name + '15.00 ')
+    console.log('moot ' + number + '5.00 ')
+
+
+}
+
+// pizzaParty('Mikle ', 'veggies ')
+
+employediscount("Ian", 100)
