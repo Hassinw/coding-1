7c360c8c615f383ec111a9baed01a3464ef7d345
Hassinw <hassinwilliams1@gmail.com>
2024-12-20 20:29:24 +0000

    friday test tried my best in the time i had

---
+++ unit 3 .js/ Quarter2_Quiz.js
@@ -0,0 +1,40 @@
+// #1 In your own words, describe what a function is. Please write your response in a complete sentence as a string data type.
+//'a function a block of code made to perfrom a simple task'
+
+// #2 In your own words, describe the difference between a function parameter and a function argument. Please write your response in a complete sentenc as a string data typee.
+//' a function perameter is a the names listed in the function definition, while a function argument is the real values passed to (and received by) the function.'
+
+// #3 ' the if statement is to be used on a peice of code if the condition is true,'
+//'while the else condition is used to  to specify a block of code to be executed, if the same condition is false'
+
+// #4 integar are data types thst represent numbers
+// #5 booleans represemt data that has a true of false value
+
+
+
+
+
+
+// #6 'operator you would use for the age verification the true or false operator'
+
+// #7 'you would use const because you would not want the secerity code to change '
+
+//#8
+function passwordcheck(password, > 10)
+console.log('')[
+if (password = 'if password is longer then 10 characters, sorry password is to long. ')
+
+else if ('if password== less than 4 character ') [
+    console.log('sorry, your password is to short')
+]
+
+]
+
+
+
+
+
+
+
+
+passwordcheck()
