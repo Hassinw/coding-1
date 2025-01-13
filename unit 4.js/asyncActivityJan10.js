59a1a8a1d06cf0d89eb7ce5e3465a82c3a5c2224
Hassinw <hassinwilliams1@gmail.com>
2025-01-13 18:57:41 +0000

    today class work worked on code and did some editing in time we had

--- unit 4.js/asyncActivityJan10.js
+++ unit 4.js/asyncActivityJan10.js
@@ -1,6 +1,6 @@
-grocery store ['meat'.'bannas','corn','pears','']
-frozen food ile['ice cream','crabs','waterice','icecreamcake',]
-produce isle ['onions','bellpeppers','carrots','cucumbers','beats']
+var internationalfood= ['meat'.'bannas','corn','pears','soy sauce']
+var frozen food ile=['ice cream','crabs','waterice','icecreamcake',]
+var produce isle= ['onions','bellpeppers','carrots','cucumbers','beats']
 
 
 console.log the number at index (0)
@@ -8,4 +8,16 @@ console.log the number at index (0)
 console.log(myArray[0]); // Output: "apple"
 
 
-function multpyer()
+function multpyer(0)[
+console.log(x * 293)
+
+
+]
+multplier(x)
+
+var numbers= [1,30,39,50,293,100]
+// 1 one starts at 0
+
+// number 3 starts at 50
+
+// number 4 293
