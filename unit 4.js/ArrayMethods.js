82cd781b818c83948537898d31ebeea1f523a576
Hassinw <hassinwilliams1@gmail.com>
2025-01-15 20:16:26 +0000

    today code worked on array methods.

---
+++ unit 4.js/ArrayMethods.js
@@ -0,0 +1,26 @@
+//data stypes all have 
+
+// JS Srrsy is a data type for collecting and storing
+// other data types, including other list.
+
+
+
+var name = 'Moo';
+var grocery ={'apple,' 'milk,''water}
+
+// An array can store 
+var gemeralist [name1,grocery,132323,23,34 true]
+
+// a method a function that WORKS on a list object
+// A method are written after the list variable name and
+// declered with a period
+// pretty much; the method is going inside of the list 
+// and running instructions on a data from the list
+
+console.log push('new data')
+console.log(generalist)
+
+// pop method() - This method removes a place of data
+// from a list. it will remove the last item in that list
+genralist.pop()
+console.log(genralist)
