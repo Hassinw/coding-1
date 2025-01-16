8ec2eba4522274e946e3ce8993b7de692c02aa69
Hassinw <hassinwilliams1@gmail.com>
2025-01-16 20:16:37 +0000

    today class work couldnt get it all

---
+++ unit 4.js/jsArrayMethods.js
@@ -0,0 +1,38 @@
+// we declere
+// variable name 
+
+
+
+
+var = snowGear=['gloves',]
+
+
+
+
+
+
+
+// what is a method 
+// a special type of function that works on list
+
+// push() - this special type of function pushes new 
+// data typed
+
+// pop this special function for removing
+// and item from an array.
+// pop does not need an argument. it will allways remove the last item in the array
+
+snowGear.pop()
+console.log(snowGear)
+
+// LIFO= Last in, First out
+
+
+// length- coounts the items in the array 
+// length does NOT take any arguments
+console.log(snowGear.length)
+
+// remove The hats string from the list 
+// use w3schools or google tp find a methood
+// to remove the hats from the array
+var snowGear=['gloves','hats','mittens','coat']
