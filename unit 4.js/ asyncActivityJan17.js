f96c6f8619b72c322ec496c6049a31bd46b5b03c
Hassinw <hassinwilliams1@gmail.com>
2025-01-17 20:05:22 +0000

    couldt figure out number 2 and 3 was behind on notes yesterday

---
+++ unit 4.js/ asyncActivityJan17.js
@@ -0,0 +1,20 @@
+//Using the list above, create a function that will reverse the order of the list/array.
+
+campingSupplies = []
+
+function reverse()
+return list.reverse()
+const= ['tent','sleeping bag','flash light','camping knife']
+ console.log(reversesupplies);= ['tent','sleeping bag','flash light','camping knife']
+
+ // Create a function that will remove the "flash light" item from the list/array and replace it with a "camp fire kit" item.
+
+ function replace(flashlight) {
+    const flashlightIndex = array.indexOf("flashlight");
+  
+    if (flashlightIndex !== -1) {
+      array[flashlightIndex] = "campfire kit";
+    }
+  
+    return array;
+  }
