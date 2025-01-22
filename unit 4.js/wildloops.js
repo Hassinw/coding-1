f2ac7c1e1438c7a4b3841230d48d3d926c766687
Hassinw <hassinwilliams1@gmail.com>
2025-01-22 20:17:30 +0000

    today classwork worked on suncng js document and html.

---
+++ unit 4.js/wildloops.js
@@ -0,0 +1,16 @@
+// loops = A loop is a sspecial 
+// constuct that repeats a set
+// of code instruction over
+// and over underspecific conditions.
+
+// while loops = this a special type of
+// loop where so long as a condition is true 
+// it will repeat the code instruction over and over
+// 1  assignment conparison same as  ==
+
+x = 2
+while (x ==2)[
+    console.log('this message will repeat forever')
+]
+
+
