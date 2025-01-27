8c4b76cb49f66e5f7379716c1e4fae0eb28705d7
Hassinw <hassinwilliams1@gmail.com>
2025-01-27 18:57:36 +0000

    today async review worked on work from friday will continue tomorrow

---
+++ unit 4.js/asyncActivityJan24.js
@@ -0,0 +1,19 @@
+function reverseNumber()[
+    var i = 0
+    while(i > 0)
+        console.log(numberList)
+    if (i<0)
+        console.log('')
+
+]
+
+
+
+
+function reversefunction()[
+    var ListNumber = 7 
+    while(ListNumber >=0 )[
+        consonsole.log(numberList[ListNumber])
+    ListNumber -=1
+    ]
+]
