853bfa6a2db1f73637b402a0fcd7a0b5b469db7a
Hassinw <hassinwilliams1@gmail.com>
2025-01-31 20:02:31 +0000

    friday async activity worked on answering the qustions and the time i had

---
+++ unit 4.js/ asyncActivityJan31.js
@@ -0,0 +1,16 @@
+// 1 In a complete sentence, what is the problem asking you to do ?
+// the problem is asking you too make a grade checker  that will pass a number percente then give you a letter grade example 70% c.
+
+// 2 Where there any parts of the question that don't make sense, if so write them down in complete sentences. 
+
+// 3 Please write down the keywords and phrases that will help you solve this problem ?
+    //  "B". Your program should then repeat by asking the user to enter another numerical grade.
+
+// 4 Does the problem provide you with any input data? Does it tell you what type of data your program needs to take in ?
+
+ // numerical grade.
+// 5 Does the problem provide you with desired output data? Does it tell you that your program needs to give somthing back to the user ?
+   // the output data it has to give back to the user is letter grade
+
+// 6 Using Pseudocode (fake code), write down Step by step how you think you can solve this problem. Remember, this does not have to be code.
+// first i would make a function then console log then enter the number percentages add another console log then add A,b,C,D
