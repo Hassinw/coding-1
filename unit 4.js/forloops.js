fb19ce1ce529c0c96eba2f41ae4aece3a837de1a
Hassinw <hassinwilliams1@gmail.com>
2025-02-05 20:14:40 +0000

    today class work = learned forloops and worked on code.

---
+++ unit 4.js/forloops.js
@@ -0,0 +1,21 @@
+// for loop is a type of loop yhat iterates  trought a list.
+// a list has a finite loop; it has a startpotimt and it will end at the last item
+
+
+
+
+groceryList = ['apple','mango','water','banna','beef']
+let i=0
+for (i; i <groceryList.length; i+=1)[
+    console.log('this is a'+ grocerystoreList)
+]
+
+let gradebookList = [80,70,74,92,68,93,92]
+let y = 0
+for (x; x < gradebookList.length; y+=1)[
+if (gradebookList[x]< 75)
+     (gradebookList[x]+ 5 )[
+
+    ]
+    console.log(gradebookList[x]+5)
+]
