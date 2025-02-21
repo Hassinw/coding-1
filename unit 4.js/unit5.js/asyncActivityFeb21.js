2d8da7e3ffecdcf0271a1a70b5a325f6eb646894
Hassinw <hassinwilliams1@gmail.com>
2025-02-21 20:12:16 +0000

    friday async acticty talked about developing games.

---
+++ unit 4.js/unit5.js/asyncActivityFeb21.js
@@ -0,0 +1,17 @@
+//1 What is the name of your game?  
+
+'falling figures'
+
+// 2 What genre is your game going to be in (horror, action-adventure, shooting, fighting, platformer, etc.) 
+
+' the genere my game is going to be in is platformer.'
+
+// 3 What is the objective and general game loop of your game? Please write your response in complete sentences. 
+
+'players will have to avoid obsticles while running. the objective of my game is to see how long you can run for wihtout getting hit in a time frame of 3 mins.'
+// 4 What already existing games does your game take inspiration from? Please provide at least 3 examples, 
+
+'my game takes insperation from fino run the game that pops up on your laptop when your internet is down im inspired by how the game is nvr ending and when you die you can just restart.'
+' the 2nd insperation is subway suffers this game is when you run from a cop chasing you and collect items to help you run faster. whatinspired me is how the items can boost your speed.'
+' the last insperation  is temple run this game you ru8n from a monster its simillar to subway suffer but what inspired me is the numerus turns and corns you can turn to get away.'
+// 5 Please describe the mechanics, or functional parts that will be in your game example:  i would use function const while loops probaly for loops.
