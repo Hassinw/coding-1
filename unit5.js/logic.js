dcfc3a7c7ce12f55d8b43c26e10bc999de7e2c0e
Hassinw <hassinwilliams1@gmail.com>
2025-03-27 19:14:29 +0000

    today game worked onm p2 and made attack button and damage.

---
+++ unit5.js/logic.js
@@ -0,0 +1,21 @@
+console.log(connected)
+
+let useerhealth = 100
+let cpuhealth = 100
+document.getElementById('userH').TextConTent=userhealth
+document.getElementById('cpuH').textContent=cpuHealthlet
+
+let fireattack 40
+let waterattack 20
+let healing 25
+
+
+
+
+
+function attack(attacktype)[
+    console.log("attack")
+    if (attackType== 'fire')[
+        fireAttack - cpiHealth
+    ]
+]
