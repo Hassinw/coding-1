adf932d35832954611f7d92fc4d7e25450c7657d
Hassinw <hassinwilliams1@gmail.com>
2025-04-09 19:16:32 +0000

    worked on installing node.

--- mario tic tac to/logic.js
+++ mario tic tac to/logic.js
@@ -1,3 +1,13 @@
-function testButton(){
+function testButton() {
     console.log('button works!')
 }
+
+function button() {
+console.log("button is clicked")
+alert('button clicked from game ')
+}
+
+console.log('JS mario')
+
+
+
