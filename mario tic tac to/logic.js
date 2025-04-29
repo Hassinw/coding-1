5e0941465b7a8bb86c2f400db8253354ffd99dae
Hassinw <hassinwilliams1@gmail.com>
2025-04-29 19:04:48 +0000

    project

--- mario tic tac to/logic.js
+++ mario tic tac to/logic.js
@@ -1,13 +1,27 @@
 function testButton() {
-    console.log('button works!')
-}
-
-function button() {
-console.log("button is clicked")
-alert('button clicked from game ')
-}
-
-console.log('JS mario')
-
-
-
+    console.log("button works!");
+  }
+  
+  function button() {
+    console.log("button is clicked");
+    alert("button clicked from game ");
+  }
+  
+  function randomizeImages() {
+    var imageNumber = Math.floor(Math.random() * 2);
+    console.log(imageNumber);
+  }
+  
+  // randomizeImages();
+  
+  console.log("JS mario");
+  
+  //  need logic to randomize picture order
+  
+  // need logic to hife and show images.
+  
+  //   when a user clicks on an image, it should show the picture.
+  // then they get the option to click on another picture.
+  
+  //  if the pictures have the same value- its a match, else: turn pictures back over.
+  
